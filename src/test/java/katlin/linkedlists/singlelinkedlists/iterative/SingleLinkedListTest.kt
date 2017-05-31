94ed6eefd275f3f66694345f687496b702f03bc6
javatlacati <scherzo_16@hotmail.com>
2017-05-31 05:03:38 -0500

    prueba unitaria para lista ligada simple en kotlin

---
+++ src/test/java/katlin/linkedlists/singlelinkedlists/iterative/SingleLinkedListTest.kt
@@ -0,0 +1,57 @@
+package katlin.linkedlists.singlelinkedlists.iterative
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Created by Core i7 on 31/05/2017.
+ */
+class SingleLinkedListTest {
+    internal var instance: SingleLinkedList? = null
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        instance = SingleLinkedList()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getStrings() {
+        instance!!.addAtEnd(Node("Adios"))
+        instance!!.addAtEnd(Node("mundo"))
+        instance!!.addAtEnd(Node("cruel"))
+        assertEquals("Node(item=Adios)Node(item=mundo)Node(item=cruel)", instance!!.strings)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun equalstest() {
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun hashCodetest() {
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun canEqual() {
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getRoot() {
+        val instance = SingleLinkedList()
+        assertNull(instance.root)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun setRoot() {
+        instance!!.root = Node(3)
+        assertNotNull(instance!!.root)
+    }
+
+}
