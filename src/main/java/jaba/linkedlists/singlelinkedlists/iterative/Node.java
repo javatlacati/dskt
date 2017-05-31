8cec22e25f841cb005f18f511e67f12dee312378
javatlacati <scherzo_16@hotmail.com>
2017-05-30 19:58:45 -0500

    Implementación básica de nopdo de lista simplemente ligado de manera iterativa

--- src/main/java/jaba/linkedlists/singlelinkedlists/iterative/Node.java
+++ src/main/java/jaba/linkedlists/singlelinkedlists/iterative/Node.java
@@ -1,7 +1,22 @@
 package jaba.linkedlists.singlelinkedlists.iterative;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Created by Core i7 on 30/05/2017.
  */
+@Setter
+@Getter
+@EqualsAndHashCode(of = {"item"})
+@ToString(of = "item")
 public class Node {
+    private Object item;
+    private jaba.linkedlists.singlelinkedlists.recursive.Node next;
+
+    public Node(Object item) {
+        this.item = item;
+    }
 }
