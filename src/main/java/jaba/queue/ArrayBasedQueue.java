1e9b242b0a8cb74720c9c7a3ef9ea09d9ef4b521
javatlacati <scherzo_16@hotmail.com>
2017-05-31 12:32:16 -0500

    Prueba unitaria para la implementación de cola basada en un arreglo

---
+++ src/main/java/jaba/queue/ArrayBasedQueue.java
@@ -0,0 +1,55 @@
+package jaba.queue;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.val;
+
+/**
+ * The time needed to add or delete an item is constant and independent of the number of items in the queue. So both addition and deletion can be O(1) operation.
+ */
+@ToString(of = "queueArray")
+public class ArrayBasedQueue {
+    @Getter
+    @Setter
+    private int capacity;
+    private Object queueArray[];
+    private int topIndex = 0;
+
+    public ArrayBasedQueue(int queueSize) {
+        this.capacity = queueSize;
+        queueArray = new Object[this.capacity];
+    }
+
+    public void enqueue(Object item) {
+        if (isFull()) {
+            throw new OutOfMemoryError("Queue Capacity has been excedeed");
+        } else {
+            queueArray[topIndex++] = item;
+        }
+
+    }
+
+    public Object dequeue() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Queue Undeflow");
+        } else {
+            val dequeued = queueArray[topIndex];
+            queueArray = null;
+            topIndex--;
+            return dequeued;
+        }
+    }
+
+    public int getCurrentSize() {
+        return capacity - topIndex;
+    }
+
+    public boolean isFull() {
+        return capacity < (topIndex + 1);
+    }
+
+    public boolean isEmpty() {
+        return topIndex == 0;
+    }
+}
