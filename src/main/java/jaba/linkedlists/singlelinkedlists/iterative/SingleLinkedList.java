08247c33449f5b119e3399cae2320b59c32e5281
javatlacati <scherzo_16@hotmail.com>
2017-05-31 03:20:29 -0500

    getter y setter para lista ligada simple iterativa

--- src/main/java/jaba/linkedlists/singlelinkedlists/iterative/SingleLinkedList.java
+++ src/main/java/jaba/linkedlists/singlelinkedlists/iterative/SingleLinkedList.java
@@ -1,7 +1,13 @@
 package jaba.linkedlists.singlelinkedlists.iterative;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Created by Core i7 on 30/05/2017.
  */
+@Setter
+@Getter
 public class SingleLinkedList {
+    private Node root;
 }
