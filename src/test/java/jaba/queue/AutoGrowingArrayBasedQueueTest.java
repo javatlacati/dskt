b68f52f0a28d25a72cc14262c98898a4d96bd48d
Ruslan López <scherzo_16@hotmail.com>
2017-06-06 01:04:21 -0500

    pruebas unitarias de la cola autoredimensionable

---
+++ src/test/java/jaba/queue/AutoGrowingArrayBasedQueueTest.java
@@ -0,0 +1,92 @@
+package jaba.queue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Administrador on 06/06/2017.
+ */
+public class AutoGrowingArrayBasedQueueTest {
+    ArrayBasedQueue queue;
+    private static final int initialCapacity = 2;
+    @Before
+    public void setUp() throws Exception {
+        queue = new AutoGrowingArrayBasedQueue(initialCapacity,2);
+    }
+
+    @Test
+    public void enqueue() throws Exception {
+    }
+
+    @Test
+    public void setCapacityIncrement() throws Exception {
+    }
+
+    @Test
+    public void getCapacityIncrement() throws Exception {
+    }
+
+    @Test
+    public void dequeue() throws Exception {
+    }
+
+    @Test
+    public void getCurrentSize() throws Exception {
+    }
+
+    @Test
+    public void isFull() throws Exception {
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+    }
+
+    @Test
+    public void setCapacity() throws Exception {
+    }
+
+    @Test
+    public void getCapacity() throws Exception {
+    }
+
+    @Test
+    public void happyPath() throws Exception {
+        System.out.println(queue);
+        queue.enqueue(4);
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+        queue.enqueue(56);
+        System.out.println(queue);
+        queue.enqueue(2);
+        System.out.println(queue);
+        queue.enqueue(67);
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+        queue.enqueue(24);
+        System.out.println(queue);
+        queue.enqueue(98);
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+        queue.enqueue(435);
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+        queue.dequeue();
+        System.out.println(queue);
+        try{
+            queue.dequeue();
+            System.out.println(queue);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
