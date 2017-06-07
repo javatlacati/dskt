169b2274693df1b50c4807093e0b6b15898caaa1
Ruslan López <scherzo_16@hotmail.com>
2017-06-07 02:36:54 -0500

    deque

---
+++ src/main/java/jaba/queue/deque/ArrayBasedDeque.java
@@ -0,0 +1,11 @@
+package jaba.queue.deque;
+
+import lombok.ToString;
+
+/**
+ * Created by Administrador on 07/06/2017.
+ */
+@ToString(of = "dequeArray")
+public class ArrayBasedDeque {
+    Object[] dequeArray;
+}
