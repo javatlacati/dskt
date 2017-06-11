9452d7a6fe6d2455a2af44310c308324e9c7520f
Ruslan López <scherzo_16@hotmail.com>
2017-06-10 21:52:37 -0500

    Pruebas de performance con datos aleatorios y se agrega prueba de performance para toArray

--- src/test/java/jaba/lists/ListPerformanceTest.java
+++ src/test/java/jaba/lists/ListPerformanceTest.java
@@ -6,15 +6,18 @@ import com.carrotsearch.junitbenchmarks.annotation.LabelType;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.ArrayList;
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Administrador on 10/06/2017.
  */
 @BenchmarkHistoryChart(filePrefix = "map-types-benchmark-history-chart", labelWith = LabelType.RUN_ID, maxRuns = 40)
 public class ListPerformanceTest {
-    private final int iterations = 5000;
+    private final int iterations = 7000;
+    final Random random = new Random();
 
     @Rule
     public BenchmarkRule benchmarkRun = new BenchmarkRule();
@@ -23,9 +26,20 @@ public class ListPerformanceTest {
     public void sizeIteraiveSingleLinkedList() throws Exception {
         size(new jaba.lists.linkedlists.singlelinkedlists.iterative.SingleLinkedList());
     }
+
     @Test
     public void sizeRecursiveSingleLinkedList() throws Exception {
         size(new jaba.lists.linkedlists.singlelinkedlists.recursive.SingleLinkedList());
+    }
+
+    @Test
+    public void toArrayIteraiveSingleLinkedList() throws Exception {
+        toArray(new jaba.lists.linkedlists.singlelinkedlists.iterative.SingleLinkedList());
+    }
+
+    @Test
+    public void toArrayRecursiveSingleLinkedList() throws Exception {
+        toArray(new jaba.lists.linkedlists.singlelinkedlists.recursive.SingleLinkedList());
     }
 
     private void size(MyList list) {
@@ -33,13 +47,23 @@ public class ListPerformanceTest {
         assertEquals(0, list.size());
 
         for (int i = 1; i < iterations; i++) {
-            list.add(i);
-            assertEquals(i,list.size());
+            list.add(Math.abs(random.nextInt()));
+            assertEquals(i, list.size());
         }
 
         list.clear();
-        assertNotNull(list.size());
         assertEquals(0, list.size());
+    }
+
+    private void toArray(MyList myList) throws Exception {
+        assertArrayEquals(new Object[0], myList.toArray());
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < iterations; i++) {
+            int randomVal = Math.abs(random.nextInt());
+            list.add(randomVal);
+            myList.add(randomVal);
+        }
+        assertArrayEquals(list.toArray(), myList.toArray());
     }
 
 }
