a8c6e854674d17371be39acdf8c631279b806a79
Ruslan López <scherzo_16@hotmail.com>
2017-06-11 11:27:51 -0500

    Se agrega Iterador a Bag

--- src/main/java/jaba/bags/ArrayBasedBag.java
+++ src/main/java/jaba/bags/ArrayBasedBag.java
@@ -1,6 +1,13 @@
 package jaba.bags;
 
-public class ArrayBasedBag implements Bag {
+import org.jetbrains.annotations.NotNull;
+import sun.plugin.dom.exception.NoModificationAllowedException;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class ArrayBasedBag implements Bag, Iterable {
 
     private Object[] internalArray;
     private int topPosition;
@@ -27,5 +34,76 @@ public class ArrayBasedBag implements Bag {
     @Override
     public int size() {
         return topPosition;
+    }
+
+    @NotNull
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            /**Index of current element.*/
+            int currentElementIdx = 0;
+
+            @Override
+            public boolean hasNext() {
+                return topPosition != internalArray.length - 1;
+            }
+
+            @Override
+            public Object next() {
+                if (currentElementIdx <= topPosition) {
+                    return internalArray[currentElementIdx++];
+                } else {
+                    return null;
+                }
+            }
+
+            @Override
+            public void remove() {
+                throw new NoModificationAllowedException("Bag can't remove elements only add");
+            }
+
+            //Java8 only
+            //@Override
+            public void forEachRemaining(Consumer action) {
+                while (hasNext())
+                    action.accept(next());
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+        iterator().forEachRemaining(action);
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return new Spliterator() {
+            @Override
+            public boolean tryAdvance(Consumer action) {
+                if (action == null)
+                    throw new NullPointerException();
+                //TODO implement
+                return false;
+            }
+
+            @Override
+            public Spliterator trySplit() {
+                //TODO implement
+                return null;
+            }
+
+            @Override
+            public long estimateSize() {
+                //TODO implement
+                return 0;
+            }
+
+            @Override
+            public int characteristics() {
+                //TODO implement
+                return 0;
+            }
+        };
     }
 }
--- src/main/java/jaba/bags/Bag.java
+++ src/main/java/jaba/bags/Bag.java
@@ -3,8 +3,8 @@ package jaba.bags;
 /**
  * Te hace falta ver mas bags.
  */
-public interface Bag {
-    void add(Object item);
+public interface Bag<Item> {
+    void add(Item item);
 
     boolean isEmpty();
 
