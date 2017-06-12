3ebdc76908c40807cd442e328f185417f93d7dcc
Ruslan López <scherzo_16@hotmail.com>
2017-06-12 00:36:22 -0500

    Prueba de ordenamiento burbuja iterativo en Java con medicion de performance

--- src/test/java/jaba/arrays/BubbleSort.java
+++ src/test/java/jaba/arrays/BubbleSort.java
@@ -1,26 +1,46 @@
 package jaba.arrays;
 
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
+import com.carrotsearch.junitbenchmarks.annotation.BenchmarkHistoryChart;
+import com.carrotsearch.junitbenchmarks.annotation.BenchmarkMethodChart;
+import com.carrotsearch.junitbenchmarks.annotation.LabelType;
 import lombok.val;
+import org.jetbrains.annotations.Contract;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Properties;
 import java.util.Random;
 import java.util.stream.IntStream;
 
+@BenchmarkMethodChart(filePrefix = "benchmark-lists2")
+@BenchmarkHistoryChart(labelWith = LabelType.RUN_ID)
 public class BubbleSort {
-    private int[] targetArray;
+
+    @Rule
+    public BenchmarkRule benchmarkRun = new BenchmarkRule();
+
     private Random r = new Random();
 
-    @Before
-    public void setUp() throws Exception {
-        targetArray = IntStream.generate(r::nextInt).limit(r.nextInt(10)).toArray();
-        System.out.println(Arrays.toString(targetArray));
+    @BeforeClass
+    public static void loadProperties() throws IOException {
+        Properties p = new Properties();
+        p.load(new FileInputStream(new File("src/test/resources/jub.properties")));
+        for (String k : p.stringPropertyNames()) {
+            System.setProperty(k, p.getProperty(k));
+        }
     }
 
     @Test
     public void randomData() throws Exception {
+        int[] targetArray = IntStream.generate(r::nextInt).limit(r.nextInt(7000)).toArray();
+        System.out.println(Arrays.toString(targetArray));
         int[] sorted = bubbleSort(targetArray);
 //        System.out.println(Arrays.toString(sorted));
         int[] sortedByJava = Arrays.copyOfRange(targetArray, 0, targetArray.length);
@@ -40,6 +60,7 @@ public class BubbleSort {
         return targetArray;
     }
 
+    @Contract(pure = true)
     private boolean isSorted(final int[] targetArray) {
         for (int i = 0; i < targetArray.length - 1; i++) {
             if (targetArray[i] > targetArray[i + 1]) {
