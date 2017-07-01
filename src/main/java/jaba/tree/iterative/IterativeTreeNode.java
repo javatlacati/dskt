54ba5feaf4ec20fbae346dc80681c861f7b22612
Ruslan López <scherzo_16@hotmail.com>
2017-07-01 11:29:11 -0500

    Se agrega espacio para nodos de arboles

---
+++ src/main/java/jaba/tree/iterative/IterativeTreeNode.java
@@ -0,0 +1,7 @@
+package jaba.tree.iterative;
+
+/**
+ * Created by Administrador on 01/07/2017.
+ */
+public class IterativeTreeNode {
+}
---
+++ src/main/java/jaba/tree/recursive/RecursiveTreeNode.java
@@ -0,0 +1,7 @@
+package jaba.tree.recursive;
+
+/**
+ * Created by Administrador on 01/07/2017.
+ */
+public class RecursiveTreeNode {
+}
