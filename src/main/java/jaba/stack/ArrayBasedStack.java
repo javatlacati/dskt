f72d8411b9a8bc789900ee348c5ca3d932c20801
Ruslan López <scherzo_16@hotmail.com>
2017-08-10 06:39:19 -0500

    Changes: * Generify of Array Based Stack. * Added constructor to load a 50 elements stack * Modified push method to comply with Java implementation of stack * Added method to test if stack is empty * Added method to search for an element linearly

--- src/main/java/jaba/stack/ArrayBasedStack.java
+++ src/main/java/jaba/stack/ArrayBasedStack.java
@@ -4,24 +4,29 @@ import lombok.Getter;
 import lombok.ToString;
 
 @ToString(of = {"stackArray", "topPosition"})
-public class ArrayBasedStack {
-    Object[] stackArray;
+public class ArrayBasedStack<Item>  { //extends Stack<Item>
+    Item[] stackArray;
     @Getter
     int topPosition;
 
     public ArrayBasedStack(int capacity) {
-        stackArray = new Object[capacity];
+        stackArray = (Item[]) new Object[capacity];
     }
 
-    public void push(Object item) {
+    public ArrayBasedStack() {
+        stackArray = (Item[]) new Object[50];
+    }
+
+    public Item push(Item item) {
         if (stackArray.length > topPosition) {
             stackArray[topPosition++] = item;
+            return item;
         } else {
             throw new IndexOutOfBoundsException("Capacity overflow");
         }
     }
 
-    public Object pop() {
+    public Item pop() {
         if (topPosition > 0) {
             return stackArray[--topPosition];
         } else {
@@ -29,9 +34,21 @@ public class ArrayBasedStack {
         }
     }
 
-    public Object peek() {
+    public Item peek() {
         return stackArray[topPosition];
     }
 
+    public boolean empty() {
+        return topPosition == 0;
+    }
 
+    public synchronized int search(Object o) {
+        int result=-1;
+        for (int i = topPosition - 1; i >= 0; i--) {
+         if (stackArray[i]==o){
+             return i+1;
+         }
+        }
+        return result;
+    }
 }
