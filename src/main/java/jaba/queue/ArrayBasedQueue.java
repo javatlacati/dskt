99cf178eb093c9bd7883c0bc4de6e605c560f991
Ruslan López <scherzo_16@hotmail.com>
2017-10-04 13:50:36 -0500

    Correction of issue #18

--- src/main/java/jaba/queue/ArrayBasedQueue.java
+++ src/main/java/jaba/queue/ArrayBasedQueue.java
@@ -1,55 +1,72 @@
 package jaba.queue;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.val;
-
 /**
  * The time needed to add or delete an item is constant and independent of the number of items in the queue. So both addition and deletion can be O(1) operation.
  */
-@ToString(of = "queueArray")
-public class ArrayBasedQueue {
-    @Getter
-    @Setter
-    int capacity;
-    Object[] queueArray;
-    int topIndex = 0;
+public class ArrayBasedQueue<Item> {
+    /**
+     * numbe of elements that the queue can hold.
+     */
+    protected int capacity;
+    /**
+     * Internal array to hold the data.
+     */
+    Item[] queueArray;
+
+    private int Index = 0;
 
     public ArrayBasedQueue(int queueSize) {
         this.capacity = queueSize;
-        queueArray = new Object[this.capacity];
+        queueArray = (Item[]) new Object[this.capacity];
     }
 
-    public synchronized void enqueue(Object item) {
+    public boolean isEmpty() {
+        return Index == 0;
+    }
+
+    public boolean isFull() {
+        return Index == capacity;
+    }
+
+    public synchronized void enqueue(Item item) {
         if (isFull()) {
             throw new OutOfMemoryError("Queue Capacity has been excedeed");
         } else {
-            queueArray[topIndex++] = item;
+            queueArray[Index++] = item;
         }
-
     }
 
-    public synchronized Object dequeue() {
+    public synchronized Item dequeue() {
         if (isEmpty()) {
             throw new IndexOutOfBoundsException("Queue Undeflow");
         } else {
-            Object dequeued = queueArray[topIndex-1];
-            queueArray[topIndex-1] = null;
-            topIndex--;
+            Item dequeued = queueArray[0];
+            final int length = queueArray.length - 1;
+            for (int i = 0; i < length; i++) {
+                queueArray[i] = queueArray[i + 1];
+            }
+            Index--;
             return dequeued;
         }
     }
 
     public int getCurrentSize() {
-        return capacity - topIndex;
+        return capacity - Index;
     }
 
-    public boolean isFull() {
-        return capacity < (topIndex + 1);
+    public void makeEmpty() {
+        Index = 0;
     }
 
-    public boolean isEmpty() {
-        return topIndex == 0;
+    public String toString() {
+        return "ArrayBasedQueue(queueArray=" + java.util.Arrays.deepToString(this.queueArray) + ")";
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
     }
 }
