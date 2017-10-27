8babae3e6b75c91ff283c1977906926b37d18ff7
Ruslan López <scherzo_16@hotmail.com>
2017-10-27 05:45:27 -0500

    Improvements on ArrayBasedBagTest

--- src/test/java/typescript/bags/ArrayBasedBagTest.ts
+++ src/test/java/typescript/bags/ArrayBasedBagTest.ts
@@ -15,21 +15,65 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import * as BagPackage from '../../../../main/java/typescript/bags/bags';
+import * as typescript from '../../../../main/java/typescript/bags/bags';
 import * as Chai from "chai";
 
 describe('Basic tests', () => {
-    it('emptyness test', () => {
-        let arrayBasedBag: BagPackage.bags.Bag<String> = new BagPackage.bags.ArrayBasedBag(5);
+    let arrayBasedBag: typescript.bags.Bag<String>;
+
+    beforeEach(function () {
+        arrayBasedBag = new typescript.bags.ArrayBasedBag(5);
+    });
+
+
+    it('isEmpty test', () => {
         const result = arrayBasedBag.size();
         Chai.expect(result).to.equal(0);
     });
 
     it('add test', () => {
-        let arrayBasedBag: BagPackage.bags.Bag<String> = new BagPackage.bags.ArrayBasedBag(5);
         arrayBasedBag.add("asd");
         const result = arrayBasedBag.size();
         Chai.expect(result).to.equal(1);
+    });
+
+    it('iterator test', () => {
+        let expected: String = "holamundo";
+
+        arrayBasedBag.add("hola");
+        arrayBasedBag.add("mundo");
+        let actual = "";
+
+        console.log("es5 doesn't support iterators");
+        // for(let el of arrayBasedBag){
+        //     actual.append(el);
+        // }
+        // Chai.expect(expected).to.equal(actual.toString());
+    });
+
+
+    it('disposeItems test', () => {
+        for (let i = 0; i < 5; i++) {
+            arrayBasedBag.add(i+"");
+        }
+        Chai.expect(5).to.equal(arrayBasedBag.size());
+        arrayBasedBag.disposeItems();
+        Chai.expect(0).to.equal(arrayBasedBag.size());
+    });
+
+
+    it('forEach test', () => {
+        arrayBasedBag.add("hola");
+        arrayBasedBag.add("mundo");
+        let list = new Array<String>(2);
+
+        console.log("es5 doesn't support iterators");
+
+        // list.push(...arrayBasedBag);
+        // arrayBasedBag.each(e->list.push(e));
+        // Chai.expect(2).to.equal(list.length);
+        // Chai.expect(list[0]).to.equal(arrayBasedBag.getAtIndex(0));
+        // Chai.expect(list[1]).to.equal(arrayBasedBag.getAtIndex(1));
     });
 
 
