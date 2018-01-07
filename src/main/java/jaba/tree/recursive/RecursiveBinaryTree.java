e52702cd966a13caa102329f51543871a6685378
javatlacati <scherzo_16@hotmail.com>
2018-01-07 17:18:55 -0600

    fixed issue: Comparing incompatible types.
    (cherry picked from commit 49fe551)

--- src/main/java/jaba/tree/recursive/RecursiveBinaryTree.java
+++ src/main/java/jaba/tree/recursive/RecursiveBinaryTree.java
@@ -1,31 +1,24 @@
 package jaba.tree.recursive;
 
-import jaba.tree.iterative.IterativeBinaryTreeNode;
-
 /**
  * Created by Administrador on 02/07/2017.
  */
 public class RecursiveBinaryTree<Item> {
     RecursiveBinaryTreeNode<Item> root;
 
-    public boolean isRootNode(IterativeBinaryTreeNode<Item> node) {
+    public boolean isRootNode(RecursiveBinaryTreeNode<Item> node) {
         return root.equals(node);
     }
 
-    public boolean isInternalNode(IterativeBinaryTreeNode<Item> node) {
-        if (!isRootNode(node)) {
-            if (node.getRight() != null || node.getLeft() != null) {
-                return true;
-            }
-        }
-        return false;
+    public boolean isInternalNode(RecursiveBinaryTreeNode<Item> node) {
+        return !isRootNode(node) && ((null != node.getRight()) || (null != node.getLeft()));
     }
 
-    public boolean isLeafNode(IterativeBinaryTreeNode<Item> node) {
-        return !isRootNode(node) && node.getRight() == null && node.getLeft() == null;
+    public boolean isLeafNode(RecursiveBinaryTreeNode<Item> node) {
+        return !isRootNode(node) && (null == node.getRight()) && (null == node.getLeft());
     }
 
-    public boolean isSiblingNode(IterativeBinaryTreeNode<Item> node1, IterativeBinaryTreeNode<Item> node2) {
+    public boolean isSiblingNode(RecursiveBinaryTreeNode<Item> node1, RecursiveBinaryTreeNode<Item> node2) {
         if (isLeafNode(node1) && isLeafNode(node2)) {
             //todo implementar con dfs
             return false;
@@ -34,11 +27,11 @@ public class RecursiveBinaryTree<Item> {
         }
     }
 
-    public boolean isParentNode(IterativeBinaryTreeNode<Item> parentCandidate, IterativeBinaryTreeNode<Item> childCandidate) {
+    public boolean isParentNode(RecursiveBinaryTreeNode<Item> parentCandidate, RecursiveBinaryTreeNode<Item> childCandidate) {
         return parentCandidate.getLeft().equals(childCandidate) || parentCandidate.getRight().equals(childCandidate);
     }
 
-    public boolean isChildNode(IterativeBinaryTreeNode<Item> childCandidate, IterativeBinaryTreeNode<Item> parentCandidate) {
+    public boolean isChildNode(RecursiveBinaryTreeNode<Item> childCandidate, RecursiveBinaryTreeNode<Item> parentCandidate) {
         return isParentNode(parentCandidate, childCandidate);
     }
 
