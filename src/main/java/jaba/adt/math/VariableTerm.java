f578e4dbc4ee80cb11f728aaa8a656c38d2a36f5
ruslan.lopez <ruslan.lopez@globant.com>
2018-03-04 14:35:56 -0600

    added test for basic variable term addition

--- src/main/java/jaba/adt/math/VariableTerm.java
+++ src/main/java/jaba/adt/math/VariableTerm.java
@@ -1,50 +1,49 @@
 package jaba.adt.math;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 /**
  * Created by Administrador on 29/06/2017.
  */
-@AllArgsConstructor
-@Setter
-@Getter
-public class VariableTerm {
-    private int coefficient;
-    private String literalPart;
-    private int grade;
+@AllArgsConstructor @Setter @Getter @EqualsAndHashCode public class VariableTerm {
+  private int coefficient;
+  private String literalPart;
+  private int grade;
 
-    @Override
-    public String toString() {
-        return ((0 == coefficient) ? "" : coefficient) + literalPart + ((0 == grade) ? "" : ("^" + grade));
+  @Override public String toString() {
+    return ((0 == coefficient) ? "" : coefficient) + literalPart + ((0 == grade) ?
+        "" :
+        ("^" + grade));
+  }
+
+  public void sum(VariableTerm otherTerm) {
+    if (literalPart.equals(otherTerm.getLiteralPart())) {
+      if (grade == otherTerm.getGrade()) {
+        coefficient += otherTerm.getCoefficient();
+      } else {
+        //binomial
+      }
     }
-
-    public void sum(VariableTerm otherTerm){
-        if(literalPart==otherTerm.getLiteralPart()){
-            if(grade==otherTerm.getGrade()){
-                coefficient+=otherTerm.getCoefficient();
-            }else{
-                //binomial
-            }
-        }
-    }
+  }
 
 
-    public Object sum1(VariableTerm otherTerm){
-        if(literalPart==otherTerm.getLiteralPart()){
-            if(grade==otherTerm.getGrade()){
-                return new VariableTerm(coefficient+otherTerm.getCoefficient(),literalPart,grade);
-            }else{
-                final Monomial monomial = new Monomial(0);
-                monomial.addTerm(this);
-                monomial.addTerm(otherTerm);
-                return monomial;
-            }
-        }
+  public Object sum1(VariableTerm otherTerm) {
+    if (literalPart.equals(otherTerm.getLiteralPart())) {
+      if (grade == otherTerm.getGrade()) {
+        return new VariableTerm(coefficient + otherTerm.getCoefficient(), literalPart, grade);
+      } else {
         final Monomial monomial = new Monomial(0);
         monomial.addTerm(this);
         monomial.addTerm(otherTerm);
         return monomial;
+      }
     }
+    final Monomial monomial = new Monomial(0);
+    monomial.addTerm(this);
+    monomial.addTerm(otherTerm);
+    return monomial;
+  }
 }
