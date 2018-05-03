e04ec934f4212db68f1ef5a15ec98201edc0c791
ruslan.lopez <ruslan.lopez@globant.com>
2018-05-03 14:17:44 -0500

    structure created for tree in iterative fashion
    (cherry picked from commit dee2030)

--- src/main/java/jaba/tree/iterative/IterativeBinaryTree.java
+++ src/main/java/jaba/tree/iterative/IterativeBinaryTree.java
@@ -3,10 +3,74 @@ package jaba.tree.iterative;
 /**
  * Created by Administrador on 02/07/2017.
  */
-public class IterativeBinaryTree<Item> {
-    IterativeBinaryTreeNode<Item> root;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-    public IterativeBinaryTreeNode<Item> getRoot() {
-        return this.root;
+/** Created by Administrador on 02/07/2017. */
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class IterativeBinaryTree<Item> {
+  private IterativeBinaryTreeNode<Item> root;
+
+  /** path size from root to node. */
+  //    int getDepth(IterativeBinaryTreeNode<Item> node){
+  // TODO do trough DFS
+  //    }
+
+  int getHeight() {
+    return root.getHeight();
+  }
+
+  //  boolean isAncestor(IterativeBinaryTreeNode possibleAncestor, IterativeBinaryTreeNode
+  // possibleDescendant){
+  //      // TODO buscar padre por DFS
+  //      IterativeBinaryTreeNode<IterativeBinaryTree> parent = null;
+  //
+  //  }
+
+  boolean areSiblings(
+      IterativeBinaryTreeNode<IterativeBinaryTree> node1,
+      IterativeBinaryTreeNode<IterativeBinaryTree> node2) {
+    // TODO buscar padre por DFS
+    IterativeBinaryTreeNode<IterativeBinaryTree> parent = null;
+    if (parent != null) {
+      return node1.equals(parent.getLeft()) && node2.equals(parent.getRight())
+          || node2.equals(parent.getLeft()) && node1.equals(parent.getRight());
+    } else {
+      return false;
     }
+  }
+  /*
+  preorder
+  inorder
+  postorder
+  * */
+
+  /** make empty */
+  public void clear() {
+    root = null;
+  }
+
+  public boolean isEmpty() {
+    return root == null;
+  }
+
+  public void merge(
+      IterativeBinaryTreeNode<Item> rootItem,
+      IterativeBinaryTree<Item> tree1,
+      IterativeBinaryTree<Item> tree2) {
+    this.root = rootItem;
+    // TODO insert all
+  }
+
+  /** returns the number of elements that conform this tree. */
+  public int size() {
+      if(root==null){
+          return 0;
+      }else{
+          return  1+root.countElements();
+      }
+  }
 }
--- src/main/java/jaba/tree/iterative/IterativeBinaryTreeNode.java
+++ src/main/java/jaba/tree/iterative/IterativeBinaryTreeNode.java
@@ -8,6 +8,32 @@ public class IterativeBinaryTreeNode<Item>  {
     private IterativeBinaryTreeNode left;
     private IterativeBinaryTreeNode right;
 
+  boolean isParent() {
+    return null != left || null != right;
+  }
+
+  boolean isLeaf() {
+    return left == null && right == null;
+  }
+
+  int getHeight() {
+    // TODO hacer for DFS
+    return 0;
+  }
+
+  IterativeBinaryTree<Item> getSubtree() {
+    return new IterativeBinaryTree<>(this);
+  }
+
+  IterativeBinaryTreeNode<Item> duplicate() {
+    // TODO implement me
+    return null;
+  }
+
+  public int countElements() {
+    // TODO implement me
+    return 0;
+  }
 
     public Item getValue() {
         return this.value;
