0bfe3447e02a34fa4eb90a55cad58134034f8c14
javatlacati <scherzo_16@hotmail.com>
2018-05-17 03:09:09 -0500

    removed typescript since it got's it's own project
    (cherry picked from commit a275091)

--- src/main/java/typescript/set/sets.ts
+++
@@ -1,8 +0,0 @@
-
-import {ArrayBasedSet as _ArrayBasedSet} from "./ArrayBasedSet";
-import {Set as _Set} from "./Set";
-
-export namespace sets{
-    export let ArrayBasedSet = _ArrayBasedSet;
-    export type Set<Item> = _Set<Item>;
-}
