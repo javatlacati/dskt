dd8970c21136ca32c7c4d0780a286740710575c8
ruslan.lopez <ruslan.lopez@globant.com>
2018-05-17 23:02:08 -0500

    added test for toArray without elements for stack
    (cherry picked from commit b57ea52)

--- src/test/java/jaba/stack/ArrayBasedStackTest.java
+++ src/test/java/jaba/stack/ArrayBasedStackTest.java
@@ -1,6 +1,9 @@
 package jaba.stack;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -54,7 +57,7 @@ public class ArrayBasedStackTest {
     } catch (Exception e) {
       log.info(e.getMessage());
     }
-    Assert.assertEquals(5, stack.getElementsCapacity());
+    assertEquals(5, stack.getElementsCapacity());
     try {
       log.info(stack.pop().toString());
       log.info(stack.pop().toString());
@@ -74,11 +77,14 @@ public class ArrayBasedStackTest {
   public void toArray() {
     stack.push(4);
     stack.push(8);
-    Assert.assertArrayEquals(new Integer[] {4, 8}, stack.toArray());
+    assertArrayEquals(new Integer[] {4, 8}, stack.toArray());
     stack.push(3);
     stack.push(89);
-    Assert.assertArrayEquals(new Integer[] {4, 8, 3, 89}, stack.toArray());
+    assertArrayEquals(new Integer[] {4, 8, 3, 89}, stack.toArray());
     stack.push(34);
-    Assert.assertArrayEquals(new Integer[] {4, 8, 3, 89, 34}, stack.toArray());
+    assertArrayEquals(new Integer[] {4, 8, 3, 89, 34}, stack.toArray());
+    stack.makeEmpty();
+    assertTrue(stack.isEmpty());
+    assertArrayEquals(new Integer[] {}, stack.toArray());
   }
 }
